4bdd7d8f97ac05e00bad582af45cc064e6d5c660
Kayvan Mazaheri <KayvanMazaheri@gMail.com>
2017-03-15 20:47:50 +0330

    update token schema

--- models/Token.js
+++ models/Token.js
@@ -15,6 +15,9 @@ var getExpireDefault = function(milliseconds) {
 
 var tokenSchema = new mongoose.Schema({
   token: { type: String, required: true, unique: true },
+  tokenType: { type: String },
+  tokenID: { type: String }
+  data: { type: Object },
   expires: { type: Date, required: true, default: getExpireDefault(3 * 60 * 60 * 1000) }
 }, schemaOptions);
 
