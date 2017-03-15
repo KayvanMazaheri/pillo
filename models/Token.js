71202e8cde983abf4a658e7561e58cea43e507b3
Kayvan Mazaheri <KayvanMazaheri@gMail.com>
2017-03-15 20:09:31 +0330

    add token model

---
+++ models/Token.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+
+var schemaOptions = {
+  timestamps: true,
+  toJSON: {
+    virtuals: true
+  }
+};
+
+var getExpireDefault = function(milliseconds) {
+  var timeObject = new Date();
+  timeObject.setTime(timeObject.getTime() + milliseconds);
+  return timeObject;
+}
+
+var tokenSchema = new mongoose.Schema({
+  token: { type: String, required: true, unique: true },
+  expires: { type: Date, required: true, default: getExpireDefault(3 * 60 * 60 * 1000) }
+}, schemaOptions);
+
+userSchema.options.toJSON = {
+  transform: function(doc, ret, options) {
+    delete ret.expires;
+  }
+};
+
+var Token = mongoose.model('Token', tokenSchema);
+
+module.exports = Token;
